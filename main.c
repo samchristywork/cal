c163457963c142faef92d79c974adf2eeb00ad38
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-10-26 21:16:52 -0500

    feat: Add empty program with some data
    This data will be useful for later when printing strings to the screen.

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+const char *header = "Su Mo Tu We Th Fr Sa";
+
+const char *months[] = {"January",   "February", "March",    "April",
+                        "May",       "June",     "July",     "August",
+                        "September", "October",  "November", "December"};
+
+int main() {
+}
