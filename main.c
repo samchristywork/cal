14156c7daa90efaed56990016cab4730dc0b3720
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-10-26 21:17:46 -0500

    feat: Add a way to generate a header
    Headers look something like this:
    
    ```
        October 2023
    ```
    
    These are a bit more deceptive than they look because we need to center
    the header.

--- main.c
+++ main.c
@@ -9,5 +9,19 @@ const char *months[] = {"January",   "February", "March",    "April",
                         "May",       "June",     "July",     "August",
                         "September", "October",  "November", "December"};
 
+char *get_header(int month, int year) {
+  int spacing_amt = (strlen(header) - sizeof(months[month - 1]) - 4) / 2;
+
+  char *line = malloc(strlen(header));
+  memset(line, ' ', strlen(header));
+
+  char date[100];
+  sprintf(date, "%s %d", months[month - 1], year);
+
+  strncpy(line + spacing_amt, date, strlen(date));
+
+  return line;
+}
+
 int main() {
 }
