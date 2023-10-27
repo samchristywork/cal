d85e1b3056ae12c0d521fa77b483d6ae170bce71
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-10-26 21:20:22 -0500

    feat: Add function for calculating days in a month
    This function takes into account leap years, and has hard-coded certain
    months.

--- main.c
+++ main.c
@@ -56,5 +56,19 @@ char *print_week(time_t *day, time_t now, time_t start_of_month,
   }
 }
 
+int days_in_month(int month, int year) {
+  if (month == 2) {
+    if (year % 4 == 0) {
+      return 29;
+    } else {
+      return 28;
+    }
+  } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+    return 30;
+  } else {
+    return 31;
+  }
+}
+
 int main() {
 }
